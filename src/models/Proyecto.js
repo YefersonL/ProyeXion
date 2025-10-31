6a03e13e3b1b6f02932166aa3bfd0f9aa928d841
JackieArizaLaMejor <arizajackelin9@gmail.com>
2025-10-30 23:57:42 -0500

    corrigiendo y probando

--- src/models/Proyecto.js
+++ src/models/Proyecto.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const ProyectoSchema = new mongoose.Schema({
   nombre: { type: String, required: true },
   descripcion: { type: String, required: true },
-  fechaInicio: { type: Date },
+  fechaInicio: { type: Date  },
   fechaFin: { type: Date },
+  estado: { type: String, required: true  },
 });
 
 module.exports = mongoose.model('Proyecto', ProyectoSchema);
